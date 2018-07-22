1d7623f7856a6dcbf93164c5f29b4ec7ce697325
Kaooot <37877491+Kaooot@users.noreply.github.com>
2018-07-22 16:17:26 +0200

    CommandSetLobby, CommandSetSpawn and CommandSetSpectator to one file

--- src/main/java/de/theamychan/endergames/command/CommandSetSpawn.java
+++ src/main/java/de/theamychan/endergames/command/CommandSetSpawn.java
@@ -7,31 +7,58 @@ import io.gomint.command.CommandSender;
 import io.gomint.command.PlayerCommandSender;
 import io.gomint.command.annotation.*;
 import io.gomint.command.validator.IntegerValidator;
+import io.gomint.command.validator.StringValidator;
 import io.gomint.entity.EntityPlayer;
 
 import java.util.Map;
 
-@Name("eg setspawn")
-@Description("Setzte den Spawn")
+@Name( "setspawn" )
+@Description( "Setze die EnderGames spawns." )
 @Permission( "endergames.setspawn" )
-@Overload({
-        @Parameter( name = "spawn", validator = IntegerValidator.class, arguments = {".*"}, optional = true )
-})
+@Overload( {
+        @Parameter( name = "spectator", validator = StringValidator.class, arguments = {".*"}, optional = true )
+} )
+@Overload( {
+        @Parameter( name = "lobby", validator = StringValidator.class, arguments = {".*"}, optional = true )
+} )
+@Overload( @Parameter( name = "id", validator = IntegerValidator.class, arguments = {".*"}, optional = true ) )
+
 public class CommandSetSpawn extends Command {
 
     @Override
-    public CommandOutput execute( CommandSender commandSender, String alias, Map<String, Object> arguments ) {
+    public CommandOutput execute( CommandSender sender, String alias, Map<String, Object> arguments ) {
         CommandOutput output = new CommandOutput();
 
-        if(commandSender instanceof PlayerCommandSender ){
-            EntityPlayer player = (EntityPlayer) commandSender;
-            Integer id = (Integer) arguments.get( "spawn" );
+        String spectator = (String) arguments.get( "spectator" );
+        String lobby = (String) arguments.get( "lobby" );
+        Integer id = (Integer) arguments.get( "id" );
 
-            EnderGames.getInstance().getLocationAPI().addLocation( player.getLocation(), "Spawn." + id, true );
-            output.success( "Du hast den Spawn " + id + " gesetzt!" );
+        if ( sender instanceof PlayerCommandSender ) {
+            EntityPlayer player = (EntityPlayer) sender;
 
+            if( spectator.equals( "" ) || spectator == null ) {
+                output.fail( "Usage: /setspawn <spectator>" );
+            } else {
+                EnderGames.getInstance().getLocationAPI().addLocation( player.getLocation(), "Spectator", true );
+                output.success( "Du hast den Spectatorspawn gesetzt!" );
+            }
+
+            if( lobby.equals( "" ) || spectator == null ) {
+                output.fail( "Usage: /setspawn <lobby>" );
+            } else {
+                EnderGames.getInstance().getLocationAPI().addLocation( player.getLocation(), "Lobby", true );
+                output.success( "Du hast den Lobbyspawn gesetzt!" );
+            }
+
+            if( id.equals( "" ) || id == null ) {
+                output.fail( "Usage: /setspawn <id>" );
+            } else {
+                EnderGames.getInstance().getLocationAPI().addLocation( player.getLocation(), "Spawn." + id, true );
+                output.success( "Du hast den Spawn " + id + " gesetzt!" );
+            }
+        } else {
+            output.fail( "Du musst diesen Command Ingame ausführen!" );
         }
-
         return output;
     }
 }
